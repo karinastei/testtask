8397f7e8caf901a39dbd6d21aab7f2de9ca0e952
Karina <karina.steinberg@voco.ee>
2023-11-16 22:29:15 +0200

    index works, not showing specific attribute yet

--- index.php
+++ index.php
@@ -1,45 +1,117 @@
-<!--index.php -->
 <?php
-include_once './controller/controller.php'
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Product Information</title>
-    <style>
-        table {
-            border-collapse: collapse;
-            width: 100%;
+include_once './config/database.php';
+
+abstract class Product
+{
+    private $SKU;
+    private $name;
+    private $price;
+    private $amount;
+    private $type;
+
+    public function __construct($SKU, $name, $price, $amount, $type)
+    {
+        $this->SKU = $SKU;
+        $this->name = $name;
+        $this->price = $price;
+        $this->amount = $amount;
+        $this->type = $type;
+    }
+
+// Getters for SKU, name, and price
+    public function getSKU()
+    {
+        return $this->SKU;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getPrice()
+    {
+        return $this->price;
+    }
+
+    public function getAmount()
+    {
+        return $this->amount;
+    }
+
+    public function getType()
+    {
+        return $this->type;
+    }
+}
+
+class Book extends Product
+{
+    private $weight;
+}
+
+class Furniture extends Product
+{
+    private $height;
+}
+
+class DVD extends Product
+{
+    private $size;
+}
+
+class ProductFactory
+{
+    public static function createProduct($type, $SKU, $name, $price, $amount)
+    {
+        $className = ucfirst(strtolower($type));
+
+        if (class_exists($className) && is_subclass_of($className, 'Product')) {
+            return new $className($SKU, $name, $price, $amount, $type);
+        } else {
+            throw new Exception("Invalid product type");
         }
-        th, td {
-            border: 1px solid #ddd;
-            padding: 8px;
-            text-align: left;
+    }
+}
+
+// Create an instance of the Database class
+$database = new Database();
+$database->connect(); // Connect to the database
+
+try {
+    $productData = $database->query("SELECT products.*, dvds.*, furniture.* FROM products LEFT JOIN dvds ON products.id = dvds.product_id LEFT JOIN furniture ON products.id = furniture.product_id; ");
+
+
+    while ($row = $productData->fetch_assoc()) {
+
+        $productType = $row['type'];
+        $productSKU = $row['SKU'];
+        $productName = $row['name'];
+        $productPrice = $row['price'];
+        $productAmount = $row['amount'];
+        // Create product object using the factory
+        $product = ProductFactory::createProduct($productType, $productSKU, $productName, $productPrice, $productAmount);
+        //uses the amount to show every product with that id
+        for ($i = 0; $i < $product->getAmount(); $i++) {
+
+            // Use the $product object as needed
+            echo "SKU: " . $product->getSKU() . "<br>";
+            echo "Name: " . $product->getName() . "<br>";
+            echo "Price: $" . $product->getPrice() . "<br>";
+            echo "Type: $" . $product->getType() . "<br>";
+            echo "Amount: $" . $product->getAmount() . "<br>";
+            echo "Amount: $" . $product->getAmount() . "<br>";
+
+            $products[] = $product;
+
         }
-        th {
-            background-color: #f2f2f2;
-        }
-    </style>
-</head>
-<body>
-<h1>Product Information</h1>
-<table>
-    <thead>
-    <tr>
-        <th>SKU</th>
-        <th>Name</th>
-        <th>Price</th>
-    </tr>
-    </thead>
-    <tbody>
-    <?php foreach ($products as $product) : ?>
-        <tr>
-            <td><?php echo $product->getSKU(); ?></td>
-            <td><?php echo $product->getName(); ?></td>
-            <td><?php echo $product->getPrice(); ?></td>
-        </tr>
-    <?php endforeach; ?>
-    </tbody>
-</table>
-</body>
-</html>
+
+    }
+    echo "<pre>";
+    print_r($products);
+    echo "</pre>";
+} catch (Exception $e) {
+    echo $e->getMessage();
+
+}
+$database->disconnect(); // Disconnect from the database
