195575432d32155b6d8fa8b25f7782431be44221
Karina <karina.steinberg@voco.ee>
2023-11-18 00:06:54 +0200

    grid

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>.product-grid {
+            display: grid;
+            padding: 10%;
+            grid-template-columns: repeat(auto-fill, minmax(200px, 1fr)); /* Adjust as needed */
+            gap: 10%; /* Adjust the gap between products */
+        }
+
+        .product-box {
+            border: 1px solid #ccc;
+            padding: 5%;
+            /* Additional styling */
+            /* You can add more styles to customize the appearance of each product box */
+        }
+    </style>
+
+</head>
 <?php
 include_once './config/database.php';
 
@@ -20,29 +42,9 @@ abstract class Product
     }
 
 // Getters for SKU, name, and price
-    public function getSKU()
+    public function getValue($value)
     {
-        return $this->SKU;
-    }
-
-    public function getName()
-    {
-        return $this->name;
-    }
-
-    public function getPrice()
-    {
-        return $this->price;
-    }
-
-    public function getAmount()
-    {
-        return $this->amount;
-    }
-
-    public function getType()
-    {
-        return $this->type;
+        return $this->{$value};
     }
 
     abstract public function getproductAttribute();
@@ -136,23 +138,11 @@ $database = new Database();
 $database->connect(); // Connect to the database
 
 try {
-    $productData = $database->query("SELECT 
-    products.*, 
-    dvds.size, 
-    furniture.height, 
-    furniture.width, 
-    furniture.length,
-    books.weight
-FROM 
-    products 
-LEFT JOIN 
-    dvds ON products.id = dvds.product_id 
-LEFT JOIN 
-    furniture ON products.id = furniture.product_id
-LEFT JOIN
-    books ON products.id = books.product_id; 
-");
-
+    $productData = $database->query("SELECT products.*, dvds.size, furniture.height, furniture.width, furniture.length, books.weight
+FROM products 
+LEFT JOIN dvds ON products.id = dvds.product_id 
+LEFT JOIN furniture ON products.id = furniture.product_id
+LEFT JOIN books ON products.id = books.product_id;");
 
     while ($row = $productData->fetch_assoc()) {
 
@@ -165,29 +155,33 @@ LEFT JOIN
         $product = ProductFactory::createProduct($productType, $productSKU, $productName, $productPrice, $productAmount);
 
         $product->fetchSpecificAttribute($row); // Fetches the specific property based on the product type
-        //uses the amount to show every product
-        for ($i = 0; $i < $product->getAmount(); $i++) {
 
-            // Use the $product object as needed
-            echo "SKU: " . $product->getSKU() . "<br>";
-            echo "Name: " . $product->getName() . "<br>";
-            echo "Price: $" . $product->getPrice() . "<br>";
-            echo "Type: " . $product->getType() . "<br>";
-            echo "Amount: " . $product->getAmount() . "<br>";
-
-            echo $product->getAttributeLabel() . ": " . $product->getproductAttribute() . "<br>";
-            echo "<br>";
+        //uses the amount to add every instance of a product
+        for ($i = 0; $i < $product->getValue('amount'); $i++) {
 
             $products[] = $product;
-
-
 
         }
 
     }
-    echo "<pre>";
-    print_r($products);
-    echo "</pre>";
+    echo "<form method='post' action='delete.php'>";
+    echo "<div class='product-grid'>";
+    foreach ($products as $product) {
+        echo "<div class='product-box'>";
+        echo "<input type='checkbox' class='delete-checkbox' name='selected_products[]' value='" . $product->getValue('SKU') . "'>";
+        echo "<p>SKU: " . $product->getValue('SKU') . "</p>";
+        echo "<p>Name: " . $product->getValue('name') . "</p>";
+        echo "<p>Price: $" . $product->getValue('price') . "</p>";
+        echo "<p>Type: " . $product->getValue('type') . "</p>";
+        echo "<p>Amount: " . $product->getValue('amount') . "</p>";
+        echo "<p>" . $product->getAttributeLabel() . ": " . $product->getproductAttribute() . "</p>";
+        echo "</div>";
+    }
+    echo "</div>";
+    // echo "<pre>";
+    // print_r($products);
+    // echo "</pre>";
+
 } catch (Exception $e) {
     echo $e->getMessage();
 
